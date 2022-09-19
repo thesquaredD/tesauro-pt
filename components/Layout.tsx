684d049a2467ef6df25c3b8184aafd987074ba7c
Diogo Diogo <diogo.seabra.diogo@gmail.com>
2022-09-19 15:36:46 +0100

    Add Google OAuth

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, Theme, useMediaQuery } from "@mui/material";
+import { useRouter } from "next/router";
+import { Header } from "./Header";
+import { MobileSearch } from "./MobileSearch";
 
 export const Layout = ({ children }: { children: any }) => {
+  const router = useRouter();
+  const isSmall = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+
   return (
-    <Box
-      display={"flex"}
-      flexDirection="column"
-      justifyContent={"flex-start"}
-      alignItems="center"
-      sx={{
-        minHeight: "100vh",
-        backgroundColor: (theme) => theme.palette.background.default,
-      }}
-    >
-      {children}
-    </Box>
+    <>
+      {router.pathname === "/" ? <></> : <Header />}
+      <Box
+        display={"flex"}
+        flexDirection="column"
+        justifyContent={"flex-start"}
+        alignItems="center"
+        sx={{
+          minHeight: "100vh",
+          backgroundColor: (theme) => theme.palette.background.default,
+        }}
+      >
+        {children}
+      </Box>
+      {isSmall && router.pathname !== "/" ? <MobileSearch /> : <></>}
+    </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,25 @@ import {
   PaletteMode,
   responsiveFontSizes,
   ThemeProvider,
-  useMediaQuery,
 } from "@mui/material";
+import { Session } from "@supabase/supabase-js";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import React from "react";
-import { Header } from "../components/Header";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { Layout } from "../components/Layout";
-import { MobileSearch } from "../components/MobileSearch";
 import "../styles/globals.css";
 import { getDesignTokens } from "../styles/theme";
+import { supabase } from "../utility/supabaseClient";
 
 export const ColorModeContext = React.createContext({
   toggleColorMode: () => {},
 });
 
+export const AuthContext = createContext<Session | null>(null);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [mode, setMode] = React.useState<PaletteMode>("dark");
+  const [session, setSession] = useState<Session | null>(null);
+  const [mode, setMode] = useState<PaletteMode>("dark");
+
   const colorMode = React.useMemo(
     () => ({
       // The dark mode switch would invoke this method
@@ -34,18 +36,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
   let theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   theme = responsiveFontSizes(theme);
-  let router = useRouter();
-  const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {router.pathname === "/" ? <></> : <Header />}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        {isSmall && router.pathname !== "/" ? <MobileSearch /> : <></>}
+        <AuthContext.Provider value={session}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AuthContext.Provider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
